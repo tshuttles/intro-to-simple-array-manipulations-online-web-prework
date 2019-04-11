7584854078d297f4ad6f20cfad8c553f4eaae51c
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-11 05:50:18 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,12 +1,4 @@
-def using_push(array, string)
-  array = ["hi"]
-  array.push("Niger")
-end 
 
-def using_unshift(array, string)
-  array = ["cat", "hi", "wow", "dog", "people"]
-  array.unshift("Brooklyn Heights")
-end
 
 def using_pop(great_hits_of_the_nineties)
    great_hits_of_the_nineties.pop
