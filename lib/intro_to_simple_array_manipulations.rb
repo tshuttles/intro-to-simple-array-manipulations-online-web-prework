6ea50ac851dba11cacc9123f3c94c5c84bb16f93
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-11 05:50:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,4 +1,12 @@
+def using_push(countries_in_western_africa, country)
+  countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
+  countries_in_western_africa.push("Niger")
+end
 
+def using_unshift(neighborhoods_in_northwest_brooklyn, neighborhoods)
+  neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
+  neighborhoods_in_northwest_brooklyn.unshift("Brooklyn Heights")
+end
 
 def using_pop(great_hits_of_the_nineties)
    great_hits_of_the_nineties.pop
